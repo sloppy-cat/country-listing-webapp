0679fcd6d396119f0fa4c1423c86eb02790d339d
sloppy-cat <js3891129@gmail.com>
2021-08-02 12:22:13 +0900

    project init (no boiler-plate)

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function App() {
+  return <div>보일러 플레이트(create-react-app 등)를 사용하지 않아야 함.</div>;
+}
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const path = require("path")
+
+module.exports = {
+  entry: {
+    app: path.join(__dirname, 'src', 'index.tsx'),
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, 'disk'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      templateParameters: {
+        env: process.env.NODE_ENV === 'production' ? '' : '[DEV]',
+      },
+      minify:
+        process.env.NODE_ENV === 'production'
+          ? {
+              collapseWhitespace: true,
+              removeComments: true,
+            }
+          : false,
+    }),
+  ],
+};
